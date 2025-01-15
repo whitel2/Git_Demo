2c08bbf59340c7b31f794d51d5d34d0deadec705
Prof-Z-BCC <zwolinskirj@sunybroome.edu>
2025-01-15 13:17:07 -0800

    This is the initial file to verify cloning a git repo for personal use.

---
+++ GitDemoProj/GitDemo.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************************************\
+*	Author: Zwolinski, R.																		  *
+*	Class: CST 113 / 133 / 202                                                                    *
+*   Project: Git Demo                                                                             *
+*                                                                                                 *
+*   Description:                                                                                  *
+*   Creating a Demo to clone from Professor's Git to Student Git.                                 *
+*                                                                                                 *
+*   Input:                                                                                        *
+*   None                                                                                          *
+*                                                                                                 *
+*   Output:                                                                                       *
+*   None                                                                                          *
+*                                                                                                 *
+\*************************************************************************************************/
+
+#pragma once
+
+#include <iostream> // Included for the input and output functions.
+
+using std::cout; // Defining which cout is used.
+
+int main()
+{
+    cout << "Hello, World!\n"; // Writing to the console.
+
+    return 0; // The program has executed as intended.
+}
